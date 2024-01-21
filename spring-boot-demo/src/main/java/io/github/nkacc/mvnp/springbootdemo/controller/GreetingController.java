f54090d6120c1e93f986e4d57c078012af72eb08
nkacc <118427377+nkacc@users.noreply.github.com>
2024-01-21 21:27:24 +0530

    typo

--- spring-boot-demo/src/main/java/io/github/nkacc/mvnp/springbootdemo/controller/GreetingController.java
+++ spring-boot-demo/src/main/java/io/github/nkacc/mvnp/springbootdemo/controller/GreetingController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreatingController {
+public class GreetingController {
 
     // localhost:8080/greeting?wizardName=harry
     @GetMapping("/greeting")
