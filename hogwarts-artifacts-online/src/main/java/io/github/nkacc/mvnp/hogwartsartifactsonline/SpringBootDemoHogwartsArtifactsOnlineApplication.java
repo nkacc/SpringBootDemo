56a3c55259643bbe6b8fedebb8a0ffee4abb2b8f
nkacc <118427377+nkacc@users.noreply.github.com>
2024-01-21 23:34:56 +0530

    [E12] Initialized Hogwarts Artifacts Online - Spring Boot Application https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.2.2&packaging=jar&jvmVersion=21&groupId=io.github.nkacc.mvnp&artifactId=hogwarts-artifacts-online&name=SpringBootDemo%3Ahogwarts-artifacts-online&description=Try%20(https%3A%2F%2Fgithub.com%2FWashingtonwei%2Fhogwarts-artifacts-online)&packageName=io.github.nkacc.mvnp.hogwarts-artifacts-online&dependencies=web,devtools,data-jpa,h2

---
+++ hogwarts-artifacts-online/src/main/java/io/github/nkacc/mvnp/hogwartsartifactsonline/SpringBootDemoHogwartsArtifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package io.github.nkacc.mvnp.hogwartsartifactsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoHogwartsArtifactsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoHogwartsArtifactsOnlineApplication.class, args);
+	}
+
+}
---
+++ hogwarts-artifacts-online/src/test/java/io/github/nkacc/mvnp/hogwartsartifactsonline/SpringBootDemoHogwartsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nkacc.mvnp.hogwartsartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoHogwartsArtifactsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
