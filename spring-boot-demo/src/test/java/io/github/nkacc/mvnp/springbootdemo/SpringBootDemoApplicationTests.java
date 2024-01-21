8c6d0175132b3750e77985e86b8c45ef42afe682
nkacc <118427377+nkacc@users.noreply.github.com>
2024-01-21 19:57:32 +0530

    [E 5.a] Spring Initializr
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.2.2&packaging=jar&jvmVersion=17&groupId=io.github.nkacc.mvnp&artifactId=spring-boot-demo&name=SpringBootDemo&description=Demo%20project%20for%20Spring%20Boot%20(https%3A%2F%2Fgithub.com%2Fnkacc%2FSpringBootDemo)&packageName=io.github.nkacc.mvnp.spring-boot-demo&dependencies=web,devtools

---
+++ spring-boot-demo/src/test/java/io/github/nkacc/mvnp/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nkacc.mvnp.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
