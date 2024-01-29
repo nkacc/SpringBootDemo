afa6961e54762f9ba277d0e2372efbfcc184a8bf
nkacc <118427377+nkacc@users.noreply.github.com>
2024-01-30 00:09:15 +0530

    Completed Test for the findById on Success.
    https://www.jetbrains.com/guide/java/tutorials/writing-tests-with-spock/given-when-then/
    https://stackoverflow.com/questions/40961057/how-to-use-mockito-with-junit-5
    https://site.mockito.org/

--- hogwarts-artifacts-online/src/test/java/io/github/nkacc/mvnp/hogwartsartifactsonline/artifact/ArtifactServiceTest.java
+++ hogwarts-artifacts-online/src/test/java/io/github/nkacc/mvnp/hogwartsartifactsonline/artifact/ArtifactServiceTest.java
@@ -1,12 +1,27 @@
 package io.github.nkacc.mvnp.hogwartsartifactsonline.artifact;
 
+import io.github.nkacc.mvnp.hogwartsartifactsonline.wizard.Wizard;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class ArtifactServiceTest {
+
+    @Mock // Mock this object, don't use real class.
+    ArtifactRepository artifactRepository;
+
+    @InjectMocks
+    ArtifactService artifactService;
 
     @BeforeEach
     void setUp() {
@@ -18,6 +33,55 @@ class ArtifactServiceTest {
 
     @Test
     void findByIdSuccess() {
+        // Given: this section sets up the initial state of the system, including any required dependencies or data.
+        /// Fake data
+        /*  "data": {
+                "id": "1250808601744904192",
+                "name": "Invisibility Cloak",
+                "description": "An invisibility cloak is used to make the wearer invisible.",
+                "imageUrl": "ImageUrl",
+                "owner": {
+                  "id": 2,
+                  "name": "Harry Potter",
+                  "numberOfArtifacts": 2
+                }
+            } */
+        Artifact fakeArtifact = new Artifact();
+        fakeArtifact.setId("1250808601744904192");
+        fakeArtifact.setName("Invisibility Cloak");
+        fakeArtifact.setDescription("An invisibility cloak is used to make the wearer invisible.");
+        fakeArtifact.setImageUrl("ImageUrl");
+
+        Wizard fackWizard = new Wizard();
+        fackWizard.setId(2);
+        fackWizard.setName("Harry Potter");
+
+        fakeArtifact.setOwner(fackWizard);
+
+        /// Mock methods
+        /* https://stackoverflow.com/questions/33546124/mockito-given-versus-when
+        The problem is that current stubbing api with canonical role of `when` word does not integrate nicely with
+        //given //when //then comments. It's because stubbing belongs to //given component of the test and not to
+        the //when component of the test. Hence, BDDMockito class introduces an alias so that you stub method calls
+        with BDDMockito.given(Object) method. Now it really nicely integrates with the given component of a BDD style test!
+        But I use the `when` anyway, for understandability. */
+        when(artifactRepository.findById(fakeArtifact.getId()))
+                .thenReturn(Optional.of(fakeArtifact));
+
+        // When: this section triggers the action or behavior that we want to test.
+        /// Testing the required method, by calling it (while mocks injected).
+        Artifact returnedArtifact = artifactService.findById(fakeArtifact.getId());
+
+        // Then: this section asserts that the expected outcome has occurred.
+        /// Asserting & Verifying, the output & behaviour of that method.
+        assertEquals(returnedArtifact.getId(), fakeArtifact.getId());
+        assertEquals(returnedArtifact.getName(), fakeArtifact.getName());
+        assertEquals(returnedArtifact.getDescription(), fakeArtifact.getDescription());
+        assertEquals(returnedArtifact.getImageUrl(), fakeArtifact.getImageUrl());
+        //// Verify artifactRepository.findById() is called exactly once with "1250808601744904192".
+        verify(artifactRepository, times(1))
+                .findById(fakeArtifact.getId());
+
     }
 
     @Test
