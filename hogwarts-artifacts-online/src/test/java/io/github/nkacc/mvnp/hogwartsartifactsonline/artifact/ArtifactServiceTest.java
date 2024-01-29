4b488e674c1ae1721b72e0763a2c277799f77495
nkacc <118427377+nkacc@users.noreply.github.com>
2024-01-29 22:06:11 +0530

    Added Test for the findById, yet to code logic.
    https://junit.org/junit5/docs/current/user-guide/
    https://testng.org/ https://stackoverflow.com/questions/38273479/junit-5-vs-testng
    
    https://en.wikipedia.org/wiki/Test-driven_development

---
+++ hogwarts-artifacts-online/src/test/java/io/github/nkacc/mvnp/hogwartsartifactsonline/artifact/ArtifactServiceTest.java
@@ -0,0 +1,27 @@
+package io.github.nkacc.mvnp.hogwartsartifactsonline.artifact;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtifactServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findByIdSuccess() {
+    }
+
+    @Test
+    void findByIdFailure() {
+    }
+
+}
